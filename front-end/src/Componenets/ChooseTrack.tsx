508aed4a95e786f0f005b923f6bde0674b47d170
Ayoob <ayoobahmed@protonmail.com>
2021-08-18 01:18:12 +0100

    Added componenets file and a choose track section

---
+++ front-end/src/Componenets/ChooseTrack.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { CSSProperties } from "react";
+import { TrackData } from "../API";
+import { useHistory } from "react-router";
+
+interface Props {
+  track: TrackData;
+  access_token: string | null;
+}
+
+const ChooseTrack: React.FC<Props> = (props) => {
+  const history = useHistory();
+  const { track, access_token } = props;
+  return (
+    <div
+      key={track.id}
+      style={styles}
+      onClick={() => {
+        history.push("track/?access_token=" + access_token + "&id=" + track.id);
+      }}
+    >
+      <img src={track.image} />
+      <p>
+        <h3>
+          {track.trackName} by {track.artist}, release date:
+          {track.date}
+        </h3>
+      </p>
+    </div>
+  );
+};
+
+const styles: CSSProperties = {
+  borderTopWidth: "5px",
+  borderTopStyle: "solid",
+  borderBottomWidth: "5px",
+  borderBottomStyle: "solid",
+  borderRightWidth: "5px",
+  borderRightStyle: "solid",
+  borderLeftWidth: "5px",
+  borderLeftStyle: "solid",
+};
+
+export default ChooseTrack;
